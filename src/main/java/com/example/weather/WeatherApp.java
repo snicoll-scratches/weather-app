0da8307de8cce19418c3f50c88baf85b6f54ab97
Stephane Nicoll <snicoll@pivotal.io>
2016-05-09 12:46:23 +0200

    Cache demonstration

--- src/main/java/com/example/weather/WeatherApp.java
+++ src/main/java/com/example/weather/WeatherApp.java
@@ -3,11 +3,13 @@ package com.example.weather;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableConfigurationProperties(WeatherAppProperties.class)
+@EnableCaching(proxyTargetClass = true)
 public class WeatherApp {
 
 	@Bean
